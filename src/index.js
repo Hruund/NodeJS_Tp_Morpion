782e1b06e4a8241ed13b5a9380540ebdaf6cfe5e
William Vasselin <43633092+Hruund@users.noreply.github.com>
2021-04-29 16:28:39 +0200

    update index.js
    add function for morpion

--- src/index.js
+++ src/index.js
@@ -6,10 +6,74 @@ import socketio from 'socketio';
 const server = express();
 const socket = socketio();
 
-const joueur1 = "";
-const joueur2 = "";
-const joueur1_symbole = "croix";
-const joueur2_symbole = "rond";
+class Joueur
+{
+    constructor(name)
+    {
+        this.name = name;
+        this.score = 0;
+    }
+}
+
+class Morpion
+{
+    constructor()
+    {
+        this.game = [[""],[""],[""]];
+    }
+}
+
+const joueur1_symbole = "X";
+const joueur2_symbole = "O";
 const state_game = false;
-const joueur_gagnant = "";
-const joueur_score = ["", 0];
+const joueur_gagnant = joueur;
+const waiting_list = [];
+const game_morpion = new Morpion();
+
+server.get('/', function (req, res) {
+
+    const joueur1 = new Joueur("jean");
+    join_waintin_list(joueur1);
+
+})
+
+function join_waintin_list(joueur)
+{
+    const size_needed = 2;
+    waiting_list.push(joueur_obj);
+
+    if (waiting_list.length >= size_needed)
+    {
+        const [a, b] = waiting_list;
+        if (game_morpion.game = "")
+        {
+            add_to_game(a, b);
+        }
+    }
+}
+
+function add_to_game(joueur1, joueur2)
+{
+    waiting_list.splice(index, 1);
+    waiting_list.splice(index, 2);
+}
+
+function add_symbol(game_morpion, x, y, symbole)
+{
+    if (!checkCoord(x) || !checkCoord(y)) {
+        throw new Error('x or y must be between 0 and 2 included.');
+    }
+    if (!is_case_empty(game_morpion, x, y)) {
+        throw new Error('x or y need to be empty.');
+    }
+}
+
+function check_coord(z)
+{
+    return z >= 0 && z <= 2;
+}
+
+function is_case_empty(game_morpion, x, y)
+{
+    return game_morpion.game[x][y] != "";
+}
