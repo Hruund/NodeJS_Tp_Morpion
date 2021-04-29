8424102cec626f4492836d8019e6bd387ebb2047
William Vasselin <43633092+Hruund@users.noreply.github.com>
2021-04-29 15:13:56 +0200

    add variable to index.js src
    add variable environnement for morpion

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ import socketio from 'socketio';
 
 const server = express();
 const socket = socketio();
+
+const joueur1 = "";
+const joueur2 = "";
+const joueur1_symbole = "croix";
+const joueur2_symbole = "rond";
+const state_game = false;
+const joueur_gagnant = "";
+const joueur_score = ["", 0];
